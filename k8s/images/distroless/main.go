9128f2bca04da08c6164b841bcbebfc063576ad8
alexey.zh <mailbox.stdio@gmail.com>
2025-05-24 21:02:20 +0500

    Update test image app

--- k8s/images/distroless/main.go
+++ k8s/images/distroless/main.go
@@ -9,11 +9,13 @@ import (
 
 func main() {
 	for {
-		log.Println(time.Now().Format(time.DateTime))
-		time.Sleep(5 * time.Second)
-		err := os.WriteFile(filepath.Join("/tmp", time.Now().Format("2006-01-02-150405")), []byte{}, 0o600)
+		now := time.Now().Format("2006-01-02-150405")
+		path := filepath.Join("/tmp", now)
+		log.Printf("writing file: %s\n", path)
+		err := os.WriteFile(path, []byte{}, 0o600)
 		if err != nil {
 			log.Printf("ERROR create file: %v", err)
 		}
+		time.Sleep(5 * time.Second)
 	}
 }
