f63dd806fba5de2267d66a371c373d1b2dfaf68f
alzhi <mailbox.stdio@gmail.com>
2026-04-05 08:42:47 +0500

    refactor: add log related flags in CLI

--- cmd/root.go
+++ cmd/root.go
@@ -13,18 +13,14 @@ import (
 
 func NewRootCmd(ctx context.Context, streams genericiooptions.IOStreams) *cobra.Command {
 	cfg := genericclioptions.NewConfigFlags(true)
+	logOpts := logger.Opts{}
 
 	rootCmd := &cobra.Command{
 		Use:          "kubectl syncpod",
 		Short:        "Download/Upload files from a PVC via temporary pod",
 		SilenceUsage: true,
 		PersistentPreRun: func(_ *cobra.Command, _ []string) {
-			// TODO: --log-level, etc...
-			logger.Init(&logger.Opts{
-				Level:     "debug",
-				Format:    "text",
-				AddSource: false,
-			})
+			logger.Init(&logOpts)
 		},
 	}
 
@@ -36,6 +32,9 @@ func NewRootCmd(ctx context.Context, streams genericiooptions.IOStreams) *cobra.
 
 	rootCmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
 	cfg.AddFlags(rootCmd.PersistentFlags())
+	rootCmd.PersistentFlags().StringVar(&logOpts.Level, "log-level", "info", "Log level (trace, debug, info, warn, error)")
+	rootCmd.PersistentFlags().StringVar(&logOpts.Format, "log-format", "text", "Log format (text, json)")
+	rootCmd.PersistentFlags().BoolVar(&logOpts.AddSource, "log-add-source", false, "Add source file/line to log output")
 
 	rootCmd.AddCommand(newDownloadCmd(ctx, cfg, streams))
 	rootCmd.AddCommand(newUploadCmd(ctx, cfg, streams))
